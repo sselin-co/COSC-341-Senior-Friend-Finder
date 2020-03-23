820e5811a9ff4229a30e130432bc3cb864fbb108
unknown <eloise.espel@gmail.com>
2020-03-23 13:46:35 -0700

    finished loading screen

---
+++ LoadingScreen.java
@@ -0,0 +1,29 @@
+package com.example.agconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 3000;            //set your time here......
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(LoadingScreen.this,MainActivity.class);
+                LoadingScreen.this.startActivity(mainIntent);
+                LoadingScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
+
